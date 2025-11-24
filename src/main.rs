d7d1fa4ed509c107be6571c10596aa8e6c10ef08
matheusgondra <matheusgondra951@gmail.com>
2025-11-24 15:18:56 -0300

    refactor: move MenuOption to utils

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,7 @@ mod product;
 
 use ui::terminal;
 use product::Product;
-
-enum MenuOption {
-    Exit = 0,
-    RegisterProduct = 1,
-    ListItems = 2,
-    SearchItem = 3,
-    AddStock = 4,
-    RemoveStock = 5,
-}
-
-impl MenuOption {
-    fn from_u8(value: u8) -> Option<MenuOption> {
-        match value {
-            0 => Some(MenuOption::Exit),
-            1 => Some(MenuOption::RegisterProduct),
-            2 => Some(MenuOption::ListItems),
-            3 => Some(MenuOption::SearchItem),
-            4 => Some(MenuOption::AddStock),
-            5 => Some(MenuOption::RemoveStock),
-            _ => None,
-        }
-    }
-}
+use utils::MenuOption;
 
 fn main() {
     loop {
--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,26 @@ pub fn get_input() -> Result<String, io::Error> {
 
     Ok(buffer.trim().to_string())
 }
+
+pub enum MenuOption {
+    Exit = 0,
+    RegisterProduct = 1,
+    ListItems = 2,
+    SearchItem = 3,
+    AddStock = 4,
+    RemoveStock = 5,
+}
+
+impl MenuOption {
+    pub fn from_u8(value: u8) -> Option<MenuOption> {
+        match value {
+            0 => Some(MenuOption::Exit),
+            1 => Some(MenuOption::RegisterProduct),
+            2 => Some(MenuOption::ListItems),
+            3 => Some(MenuOption::SearchItem),
+            4 => Some(MenuOption::AddStock),
+            5 => Some(MenuOption::RemoveStock),
+            _ => None,
+        }
+    }
+}
